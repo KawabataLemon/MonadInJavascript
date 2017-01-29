7184dc581f38dd9113d6139cd06cf0ee5983e3fa
Kawabata Lemon <fresh.mosaic@gmail.com>
2017-01-29 22:50:06 +0900

    add IO Monad test read File

--- src/IO.js
+++ src/IO.js
@@ -1,12 +1,22 @@
 // IOモナド
 class IO {
 
-  constructor(fn) {
+  constructor(fn, world = null) {
     this.fn = fn
+    this.world = null
+  }
+
+  run() {
+    return this.fn()
   }
 
   chain(f) {
-    return f(this.fn())
+    return f(this.run())
+  }
+
+  static readFile(path) {
+    const fs = require('fs')
+    return new IO(()=>fs.readFileSync(path, 'utf8'))
   }
 
   static pure(value) {
--- test/test.js
+++ test/test.js
@@ -12,3 +12,4 @@ expect(Identity.value(1).chain((val) => val*3)).to.eql(3)
 
 // IO
 expect(IO.pure(3).chain(val => val*3)).to.eql(9)
+expect(IO.readFile('resource/test.txt').run()).to.eql('testtext\n')
